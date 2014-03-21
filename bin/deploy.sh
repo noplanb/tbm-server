ac9f4fd8f44f834c6c0b81ec264b9b79c6a6502f
etcetc <ff@onebeat.com>
2014-03-20 22:20:31 -0700

    remove the sqlite3 databases from deployment

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,6 @@
+# Can't add the --delete flag because it removes the production database
+# Need to add the database to mysql
 rsync -av -C --rsh 'ssh -i aws-micro.pem -l threebyme' \
   --exclude-from=exclude_from_deploy.txt \
-  --delete ./ threebyme.com:server/
+  ./ threebyme.com:server/
 ssh -i aws-micro.pem threebyme@threebyme.com 'touch server/tmp/restart.txt'
