fb12dbbba7bf1bbf4c5578e248d2467efd089db9
Sani Elfishawy <sani@sbcglobal.net>
2014-06-13 22:22:26 -0700

    deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,6 @@
 # Can't add the --delete flag because it removes the production database
 # Need to add the database to mysql
-rsync -av -C --rsh 'ssh -i aws-micro.pem -l threebyme' \
+rsync -av -C --rsh 'ssh -l threebyme' \
   --exclude-from=exclude_from_deploy.txt \
   ./ threebyme.com:server/
-ssh -i aws-micro.pem threebyme@threebyme.com 'touch server/tmp/restart.txt'
+ssh threebyme@threebyme.com 'touch server/tmp/restart.txt'
