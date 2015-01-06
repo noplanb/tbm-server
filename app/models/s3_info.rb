78948e580ce776c1a8e9b874e49e1166f07847de
Sani Elfishawy <elfishawy.sani@gmail.com>
2015-01-05 19:45:51 -0800

    Enum on region to avoid errors

--- app/models/s3_info.rb
+++ app/models/s3_info.rb
@@ -1,6 +1,17 @@
 class S3Info < ActiveRecord::Base
   
+  include EnumHandler
+  
+  define_enum :region, [:us_east_1, :us_west_1, :us_west_2, :ap_southeast_1, :ap_southeast_2, :ap_northeast_1, :sa_east_1]
+  
   def only_app_attributes
-    attributes.symbolize_keys.slice(:region, :bucket, :access_key, :secret_key)
+    r = attributes.symbolize_keys.slice(:bucket, :access_key, :secret_key)
+    r[:region] = region.to_s
+    r
   end
+  
+  def region_str
+    region.to_s
+  end
+  
 end
