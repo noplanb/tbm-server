a3d5470b62b081587d6f9270f159012c2f5b5347
Sani Elfishawy <elfishawy.sani@gmail.com>
2015-01-07 16:46:35 -0800

    Tweak to region error handling using validation rather than enum

--- app/models/s3_info.rb
+++ app/models/s3_info.rb
@@ -1,8 +1,7 @@
 class S3Info < ActiveRecord::Base
   
-  include EnumHandler
   
-  define_enum :region, [:us_east_1, :us_west_1, :us_west_2, :ap_southeast_1, :ap_southeast_2, :ap_northeast_1, :sa_east_1]
+  validates :region, inclusion: { in: region_list=%w(us_east_1 us_west_1 us_west_2 ap_southeast_1 ap_southeast_2 ap_northeast_1 sa_east_1), message: "bust either: #{region_list.join(', ')}" }
   
   def only_app_attributes
     r = attributes.symbolize_keys.slice(:bucket, :access_key, :secret_key)
