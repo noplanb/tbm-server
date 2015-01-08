334b49ca51b0e00e2a7968783786b4b1910156d5
Sani Elfishawy <elfishawy.sani@gmail.com>
2015-01-07 16:54:54 -0800

    Tweak s3_info validations

--- app/models/s3_info.rb
+++ app/models/s3_info.rb
@@ -2,6 +2,7 @@ class S3Info < ActiveRecord::Base
   
   
   validates :region, inclusion: { in: region_list=%w(us_east_1 us_west_1 us_west_2 ap_southeast_1 ap_southeast_2 ap_northeast_1 sa_east_1), message: "bust either: #{region_list.join(', ')}" }
+  validates :region, :bucket, :access_key, :secret_key, presence: true
   
   def only_app_attributes
     r = attributes.symbolize_keys.slice(:bucket, :access_key, :secret_key)
