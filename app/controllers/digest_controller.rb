d93286c576cd22429dbfa80cebc03154c303cb0a
Sani Elfishawy <elfishawy.sani@gmail.com>
2015-01-10 08:09:53 -0800

    Digest test controller tweaks

--- app/controllers/digest_controller.rb
+++ app/controllers/digest_controller.rb
@@ -7,11 +7,16 @@ class DigestController < ApplicationController
   before_action :authenticate, except: [:open]
 
   def open
-    render json: {status: "Open anyone can see me!"}
+    puts "open"
+    render json: {message: "Open anyone can get this!"}
   end
 
   def secure
-    render json: {status: "Well done you gained secure access!"}
+    msg = "Nicely done. You gained secure access from a browser. Now try from a mobile application"
+    random_word = %w{dog cat mouse beet sugar help sound carry over under above frankly}.sample
+    msg = "Well done Serhii! You got in from android! I am impressed that you succeeded with something you haven't done before! Random:#{random_word}" if /android/i =~ request.env["HTTP_USER_AGENT"]
+    
+    render json: {message: msg}
   end
 
   private
