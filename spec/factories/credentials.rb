3e640014acab0cca15b63473d4db63127d33be6b
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-03-02 20:04:08 +0200

    Added :s3_credential factory

--- spec/factories/credentials.rb
+++ spec/factories/credentials.rb
@@ -3,4 +3,11 @@ FactoryGirl.define do
     sequence(:cred_type)  { |n| "cred_type_#{n}" }
     cred 'cred'
   end
+
+  factory :s3_credential, class: S3Credential do
+    region 'us-west-1'
+    bucket 'bucket'
+    access_key 'access_key'
+    secret_key 'secret_key'
+  end
 end
