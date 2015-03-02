b91be1e835f292cb194935008297116f7c54206b
Sani Elfishawy <elfishawy.sani@gmail.com>
2015-03-02 13:56:48 -0800

    Tweaks to Figaro field names

--- config/initializers/action_mailer_init.rb
+++ config/initializers/action_mailer_init.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   port: Figaro.env.smtp_port.to_i,
   user_name: Figaro.env.smtp_user_name,
   password: Figaro.env.smtp_password,
-  domain: Figaro.env.helo_domain_name,
+  domain: Figaro.env.smtp_helo_domain_name,
   authentication: :plain,
   enable_starttls_auto: true
 }
