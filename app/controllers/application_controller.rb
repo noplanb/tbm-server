34ed318bdb26c1631044cb653124bd79c7714520
Sani Elfishawy <elfishawy.sani@gmail.com>
2015-03-06 15:29:10 -0800

    Not Found for debug reg if user not found by mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
+
   # ==============================
   # = User Agent related methods =
   # ==============================
--- app/controllers/reg_controller.rb
+++ app/controllers/reg_controller.rb
@@ -1,51 +1,51 @@
 class RegController < ApplicationController
   skip_before_filter :verify_authenticity_token
   before_filter :authenticate, except: [:reg, :debug_get_user]
-  
-  def reg  
+
+  def reg
     raw_phone = params[:mobile_number]
-    
+
     if !GlobalPhone.validate raw_phone
       Rails.logger.error("reg/reg: bad_phone")
       render json: {status:"failure", title:"Bad Phone", msg:"Please enter a valid country code and phone number"}
       return
     end
-    
+
     if (params[:device_platform].blank?)
       Rails.logger.error("ERROR: reg/reg: no device_platform: #{params.inspect}")
       render json: {status:"failure", title:"No Platform", msg:"No device_platform"}
       return
     end
-    
+
     params[:mobile_number] = GlobalPhone.normalize raw_phone
-       
+
     user = User.find_by_mobile_number(params[:mobile_number]) || User.create(user_params)
-    
+
     if (!user)
       Rails.logger.error("ERROR: reg/reg: could not find or create user: #{params.inspect}")
       render json: {status:"failure", title:"Can't Add", msg:"Unable to create user"}
       return
     end
-    
+
     render json: {status:"success", auth:user.auth, mkey:user.mkey}
     SmsManager.new.send_verification_sms(user)
   end
-  
+
   def debug_get_user
     mobile = params[:country_code].blank? ? params[:mobile_number] : "+#{params[:country_code]}#{params[:mobile_number]}"
-    user = User.find_by_mobile_number(mobile) || User.first
+    user = User.where(mobile_number: mobile).first || not_found
     render json: {status: "success"}.merge(user.only_app_attrs_for_user)
   end
-  
+
   def verify_code
     if @user && @user.passes_verification(params[:verification_code])
-      
+
       if (params[:device_platform].blank?)
         Rails.logger.error("ERROR: reg/reg: no device_platform: #{params.inspect}")
         render json: {status:"failure", title:"No Platform", msg:"No device_platform"}
         return
       end
-      
+
       # Update first and last here in case user decided to change his name to correct it
       # or something when logging in again.
       @user.update_attributes first_name: params[:first_name], last_name: params[:last_name], status: :verified, device_platform: params[:device_platform]
@@ -54,7 +54,7 @@ class RegController < ApplicationController
       render json: {status: "failure"}
     end
   end
-  
+
   def get_friends
     if @user
       render json: @user.connected_users.map{|u| u.only_app_attrs_for_friend_with_ckey(@user)}
@@ -62,9 +62,9 @@ class RegController < ApplicationController
       render json: []
     end
   end
-  
+
   private
-  
+
   def user_params
     params.permit(:first_name, :last_name, :mobile_number, :device_platform, :status, :verification_code)
   end
