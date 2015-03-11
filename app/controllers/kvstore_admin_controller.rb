a33ff97b628e3cd70dd61dda26e5f66a2fd9864d
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-03-11 02:56:48 +0200

    Restyled index pages

--- app/controllers/kvstore_admin_controller.rb
+++ app/controllers/kvstore_admin_controller.rb
@@ -2,6 +2,7 @@ class KvstoreAdminController < ApplicationController
   http_basic_authenticate_with name: Figaro.env.http_basic_username, password: Figaro.env.http_basic_password
 
   def index
+    @kvstores = Kvstore.all
   end
 
   def delete_all
