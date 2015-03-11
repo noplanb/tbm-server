30380c99562b429628ec887438b9a73e937266d3
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-03-11 03:55:25 +0200

    Pretty device platform in scafolding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def status_tag(status)
     content_tag :span, status, class: ['status', status]
   end
+
+  def human_device_platform(platform)
+    t(platform, scope: :device_platforms)
+  end
 end
