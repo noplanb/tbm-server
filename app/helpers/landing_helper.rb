151b6ccede357bfbc3581c6b7edeb3cb583d9cb9
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-03-13 20:09:26 +0200

    Moved helpers to LandingHelper.

--- app/helpers/landing_helper.rb
+++ app/helpers/landing_helper.rb
@@ -1,14 +1,33 @@
 module LandingHelper
+  # ==============================
+  # = User Agent related methods =
+  # ==============================
+  def mobile_device?
+    request.user_agent =~ /mobile|webos/i
+  end
+
+  def android?
+    request.user_agent =~ /android/i
+  end
+
+  def ios?
+    request.user_agent =~ /ios/i
+  end
+
   def app_name
     APP_CONFIG[:app_name]
   end
 
   def iphone_store_url
-    APP_CONFIG[:iphone_store_url]
+    # APP_CONFIG[:iphone_store_url]
+    ios_path
   end
 
   def android_store_url
     APP_CONFIG[:android_store_url]
   end
 
+  def store_url
+    ios? ? iphone_store_url : android_store_url
+  end
 end
