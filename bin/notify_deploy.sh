0fe62f4af5b6f91237f9bb0e89b38acaacf7e143
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-03-23 17:03:46 +0200

    Use short commit in message in notify_deploy.sh

--- bin/notify_deploy.sh
+++ bin/notify_deploy.sh
@@ -7,7 +7,7 @@ revision=$(git rev-parse HEAD)
 channel="#dev"
 username="Elastic Beanstalk"
 commit_url="${repo_url}/commit/${revision}"
-text="Deploying <${commit_url}|${revision}> to Zazo *${environment}* environment"
+text="Deploying <${commit_url}|${revision:0:8}> to Zazo *${environment}* environment"
 echo "${text} -> ${channel} @ ${username}"
 curl -X POST --data-urlencode "payload={\"text\": \"${text}\", \"channel\": \"${channel}\", \"username\": \"${username}\"}" https://hooks.slack.com/services/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
 bundle exec rake airbrake:deploy TO=${environment} REVISION=${revision} REPO=${repo_url}
