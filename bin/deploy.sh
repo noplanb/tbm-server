3fba61691972262689358fc10d836613e7469ad7
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-03-26 00:24:13 +0200

    Updated `bin/deploy.sh` script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,21 @@
-# Can't add the --delete flag because it removes the production database
-# Need to add the database to mysql
-rsync -av --delete -C --rsh 'ssh -l threebyme' \
-  --exclude-from=exclude_from_deploy.txt \
-  ./ threebyme.com:server/
-ssh threebyme@threebyme.com 'touch server/tmp/restart.txt'
+#!/bin/sh
+environment=$1
+[ -z ${environment} ] && environment=${RACK_ENV}
+[ -z ${environment} ] && environment="staging"
+repo_url="https://github.com/noplanb/tbm-server"
+revision=$(git rev-parse HEAD)
+channel="#dev"
+username="Elastic Beanstalk"
+commit_url="${repo_url}/commit/${revision}"
+revision_short=${revision:0:8}
+case ${environment} in
+  production) eb_environment="zazo-prod2-0-1"
+    ;;
+  *) eb_environment="zazo-${environment}"
+    ;;
+esac
+text="Deploying <${commit_url}|${revision_short}> to Zazo *${environment}* -> *${eb_environment}* by ${USER}"
+echo "Deploying ${revision_short} to Zazo ${environment} -> ${eb_environment} by ${USER}"
+curl -X POST --data-urlencode "payload={\"text\": \"${text}\", \"channel\": \"${channel}\", \"username\": \"${username}\"}" https://hooks.slack.com/services/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
+eb deploy ${eb_environment}
+bundle exec rake airbrake:deploy TO=${environment} REVISION=${revision} REPO=${repo_url}
