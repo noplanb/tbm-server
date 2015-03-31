e1158596e832ff55ea5db1120ca8d6a2a111337f
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-03-31 16:51:23 +0300

    Added spec for `GcmServer`

--- spec/models/gcm_server_spec.rb
+++ spec/models/gcm_server_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe GcmServer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class }
+  let(:ids) { 'push_token' }
+  let(:data) { { foo: 'bar' } }
+  let(:payload) { { registration_ids: [ids], data: data } }
+
+  describe '.make_payload' do
+    subject { described_class.make_payload(ids, data) }
+    it { is_expected.to eq(payload) }
+  end
+
+  describe '.send_notification' do
+    subject { described_class.send_notification(ids, data) }
+
+    context 'with wrong registration_ids' do
+      before do
+        VCR.use_cassette('gcm_send_with_error', erb: { key: 'gcmkey', payload: payload }) do
+          subject
+        end
+      end
+
+      it { is_expected.to be_a(Net::HTTPResponse) }
+
+      context 'body' do
+        subject { JSON.parse(described_class.send_notification(ids, data).body) }
+        let(:response_body) do
+          { 'multicast_id' => 4_843_761_582_852_144_534,
+            'success' => 0,
+            'failure' => 1,
+            'canonical_ids' => 0,
+            'results' =>
+              [{ 'error' => 'InvalidRegistration' }] }
+        end
+
+        it { is_expected.to eq(response_body) }
+      end
+    end
+  end
 end
