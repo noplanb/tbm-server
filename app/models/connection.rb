f86e9a13e643df3bbc22020af24a35e78f9c7f52
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-04-17 15:01:36 +0300

    Code style update for `Connection` and `Kvstore`

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,46 +1,45 @@
-class Connection < ActiveRecord::Base
-  require "no_plan_b/utils/text_utils"
-  include EnumHandler
-    
-  belongs_to :creator, :class_name => 'User'
-  belongs_to :target, :class_name => 'User'
-  
-  validates_presence_of :creator_id, :on => :create
-  validates_presence_of :target_id, :on => :create
-  validates_presence_of :status, :on => :create
-  
-  before_create :check_for_dups
-  
-  after_create :set_ckey
-  
-  define_enum :status,[:established, :voided],
-                       :sets => {
-                         :live => [:established]
-                       },
-                       :primary => true
+require 'no_plan_b/utils/text_utils'
 
-  scope :for_user_id, lambda{|user_id| where ["creator_id = ? OR target_id = ?", user_id, user_id]}
-  scope :between_creator_and_target, lambda{|creator_id, target_id| where ["creator_id = ? AND target_id = ?", creator_id, target_id]}
-  
+class Connection < ActiveRecord::Base
+  include EnumHandler
+
+  belongs_to :creator, class_name: 'User'
+  belongs_to :target, class_name: 'User'
+
+  validates :creator_id, :target_id, :status, presence: true, on: :create
+
+  before_create :check_for_dups
+
+  after_create :set_ckey
+
+  define_enum :status, [:established, :voided],
+              sets: {
+                live: [:established]
+              },
+              primary: true
+
+  scope :for_user_id, ->(user_id) { where ['creator_id = ? OR target_id = ?', user_id, user_id] }
+  scope :between_creator_and_target, ->(creator_id, target_id) { where ['creator_id = ? AND target_id = ?', creator_id, target_id] }
+
   def self.live_between(user1_id, user2_id)
     between_creator_and_target(user1_id, user2_id).live + between_creator_and_target(user2_id, user1_id).live
   end
-  
+
   def self.between(user1_id, user2_id)
     between_creator_and_target(user1_id, user2_id) + between_creator_and_target(user2_id, user1_id)
   end
-  
+
   def self.find_or_create(creator_id, target_id)
     between(creator_id, target_id).first || create(creator_id: creator_id, target_id: target_id, status: :established)
   end
-  
+
   def check_for_dups
-    raise "Cannot create a connection between #{creator_id} and #{target_id} a live one already exists." unless Connection.live_between(creator_id, target_id).blank?
+    unless Connection.live_between(creator_id, target_id).blank?
+      fail "Cannot create a connection between #{creator_id} and #{target_id} a live one already exists."
+    end
   end
-  
+
   def set_ckey
     update_attribute(:ckey, "#{creator_id}_#{target_id}_#{NoPlanB::TextUtils.random_string(20)}")
   end
-  
-  
 end
--- app/models/kvstore.rb
+++ app/models/kvstore.rb
@@ -1,17 +1,15 @@
 class Kvstore < ActiveRecord::Base
-  
   def self.create_or_update(params)
     if params[:key2].blank?
-      kvs = where("key1 = ? and key2 is null", params[:key1]) 
+      kvs = where('key1 = ? and key2 is null', params[:key1])
     else
-      kvs = where("key1 = ? and key2 = ?", params[:key1], params[:key2])
+      kvs = where('key1 = ? and key2 = ?', params[:key1], params[:key2])
     end
-    
+
     if !kvs.blank?
       kvs.first.update_attribute(:value, params[:value])
     else
       create(key1: params[:key1], key2: params[:key2], value: params[:value])
     end
   end
-  
 end
