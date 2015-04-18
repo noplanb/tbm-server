ef46900c0b6e4c41878e73e6a63ccdfb3e55da55
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-04-18 16:19:52 +0300

    Fixed verification_code_controller_spec.rb

--- spec/controllers/verification_code_controller_spec.rb
+++ spec/controllers/verification_code_controller_spec.rb
@@ -1,56 +1,56 @@
 require 'rails_helper'
 
-RSpec.configure do |c|
-  c.include TwimlHelpers
-end
-
 RSpec.describe VerificationCodeController, type: :controller do
-
   describe 'GET #say_code' do
     let(:valid_mobile) { '+16507800144' }
-    let(:params){ {} }
-
-    before do
-      create(User, { mobile_number: valid_mobile })
-      get :say_code, params
-    end
+    let(:params) { {} }
+    let!(:user) { create(:user, mobile_number: valid_mobile) }
 
     context 'user found with to: mobile number' do
-      let(:params){ { To: valid_mobile } }
-      it('says verification code') { expect(twiml_says_verification_code? response.body) }
+      let(:params) { { To: valid_mobile } }
+      specify do
+        get :say_code, params
+        expect(response.body).to say_twiml_verification_code
+      end
     end
 
     context 'to: not in params' do
-      it('says error') do
-        pending('FIXME: Alex, why doesnt expect rollbar error work')
-        # expect(Rollbar).to receive(:error)
-        expect(twiml_says_error? response.body)
+      specify do
+        get :say_code, params
+        expect(response.body).to say_twiml_error
+      end
+      specify do
+        expect(Rollbar).to receive(:error)
+        get :say_code, params
       end
     end
 
     context 'no user found for to: number' do
-      let(:params){ { To: '+6505551212' } }
-      it('says error') do
-        pending('FIXME: Alex, why doesnt expect rollbar error work')
-        # expect(Rollbar).to receive(:error)
-        expect(twiml_says_error? response.body)
+      let(:params) { { To: '+6505551212' } }
+      specify do
+        get :say_code, params
+        expect(response.body).to say_twiml_error
+      end
+      specify do
+        expect(Rollbar).to receive(:error)
+        get :say_code, params
       end
     end
 
     context 'no user found for to: number' do
-      it('says error') do
-        pending('FIXME: Alex, why doesnt expect rollbar error work')
-        # expect(Rollbar).to receive(:error)
-        expect(twiml_says_error? response.body)
+      specify do
+        get :say_code, params
+        expect(response.body).to say_twiml_error
+      end
+      specify do
+        expect(Rollbar).to receive(:error)
+        get :say_code, params
       end
     end
-
   end
-
 
   describe '#spaced_code' do
-    subject { controller.instance_eval{ spaced_code('1234') } }
+    subject { controller.instance_eval { spaced_code('1234') } }
     it { is_expected.to eq(' 1 2 3 4 ') }
   end
-
 end
