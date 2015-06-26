a7c7722469144d666a9d392f9de684058dac247a
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-06-26 14:34:13 +0300

    Use one migration for `users.emails` column
    :wrench: db/seeds.rb refactoring

--- db/migrate/20150624105324_rename_email_to_emails_in_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameEmailToEmailsInUsers < ActiveRecord::Migration
-  def change
-    rename_column :users, :email, :emails
-  end
-end
--- db/migrate/20150624105446_set_users_emails_type_to_text.rb
+++
@@ -1,5 +0,0 @@
-class SetUsersEmailsTypeToText < ActiveRecord::Migration
-  def change
-    change_column :users, :emails, :text
-  end
-end
