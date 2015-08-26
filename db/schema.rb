bf73ff94de598533d96ec28c1782dd4e1fd20402
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-08-26 19:46:14 +0300

    :beetle: Change *kvstores.value* column to type `text`
    Fix https://rollbar.com/zazo/zazo-server/items/88/

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624105446) do
+ActiveRecord::Schema.define(version: 20150826164305) do
 
   create_table "connections", force: :cascade do |t|
     t.integer  "creator_id", limit: 4
@@ -26,19 +26,10 @@ ActiveRecord::Schema.define(version: 20150624105446) do
   add_index "connections", ["creator_id"], name: "index_connections_on_creator_id", using: :btree
   add_index "connections", ["target_id"], name: "index_connections_on_target_id", using: :btree
 
-  create_table "credentials", force: :cascade do |t|
-    t.string   "cred_type",  limit: 255
-    t.text     "cred",       limit: 65535
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "credentials", ["cred_type"], name: "index_credentials_on_cred_type", using: :btree
-
   create_table "kvstores", force: :cascade do |t|
     t.string   "key1",       limit: 255
     t.string   "key2",       limit: 255
-    t.string   "value",      limit: 255
+    t.text     "value",      limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
   end
