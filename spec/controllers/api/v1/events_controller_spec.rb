d3d4243ad74069b3a599b99278c43119c3bbdceb
Ivan Kornilov <vano468@gmail.com>
2015-10-09 16:01:30 +0300

    Fix specs for previous changes

--- spec/controllers/api/v1/events_controller_spec.rb
+++ spec/controllers/api/v1/events_controller_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe Api::V1::EventsController, type: :controller do
   describe 'POST #create' do
     use_vcr_cassette 's3_get_metadata'
-    before { post :create, params }
+    before do
+      allow_any_instance_of(Kvstore).to receive(:trigger_event).and_return true
+      post :create, params
+    end
 
     context 'correct s3 event' do
       let(:params) { json_fixture('s3_event') }
