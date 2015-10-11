9d7873d3914f7bc9a5e445f67b4355545bf63342
Ivan Kornilov <vano468@gmail.com>
2015-10-11 20:19:48 +0300

    Implement HandleOutgoingVideo#send_notification_to_receiver

--- app/services/handle_outgoing_video.rb
+++ app/services/handle_outgoing_video.rb
@@ -3,6 +3,7 @@ class HandleOutgoingVideo
 
   def initialize(s3_event_params)
     @s3_event = S3Event.new s3_event_params
+    @errors   = {}
   end
 
   def do
@@ -10,10 +11,13 @@ class HandleOutgoingVideo
     @s3_metadata = S3Metadata.create_by_event s3_event
     handle_outgoing_video if client_version_correspond?
     true
+  rescue ActiveRecord::RecordNotFound => e
+    @errors[:mkeys] = e.message
+    false
   end
 
   def errors
-    s3_event.errors.messages
+    @errors.merge s3_event.errors.messages
   end
 
   private
@@ -24,12 +28,14 @@ class HandleOutgoingVideo
   end
 
   def update_kvstore_with_video_id
-    Kvstore.create_or_update key1: "#{s3_event.file_name}-VideoIdKVKey", key2: s3_metadata.video_id,
-                             value: { 'videoId' => s3_metadata.video_id }.to_json
+    Kvstore.create_or_update key1: "#{s3_event.file_name}-VideoIdKVKey", key2: s3_metadata.video_id, value: { 'videoId' => s3_metadata.video_id }.to_json
   end
 
   def send_notification_to_receiver
-
+    push_user = PushUser.find_by! mkey: s3_metadata.receiver_mkey
+    current_user  = User.find_by! mkey: s3_metadata.sender_mkey
+    params = { target_mkey: s3_metadata.receiver_mkey, from_mkey: current_user.mkey, sender_name: current_user.name, video_id: s3_metadata.video_id }
+    Notification::VideoReceived.new(push_user, Figaro.env.domain_name, current_user).process(params, params[:from_mkey], params[:sender_name], params[:video_id])
   end
 
   def client_version_correspond?
--- spec/services/handle_outgoing_video_spec.rb
+++ spec/services/handle_outgoing_video_spec.rb
@@ -1,23 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe HandleOutgoingVideo do
+  use_vcr_cassette 'gcm_send_with_error', erb: {
+    key: Figaro.env.gcm_api_key,
+    payload: GcmServer.make_payload(
+      'qq64zz709r4zw1l6ap5p',
+      type: 'video_received',
+      from_mkey: 'ZcAK4dM9S4m0IFui6ok6',
+      video_id: '1444235919617')
+  }
+
   describe '#do' do
-    use_vcr_cassette 's3_get_metadata'
+    context 'success case' do
+      use_vcr_cassette 's3_get_metadata'
 
-    let(:instance) { described_class.new s3_event_params }
-    let(:s3_event_params) { json_fixture('s3_event')['Records'] }
-    subject { instance.do }
+      let(:instance) { described_class.new s3_event_params }
+      let(:s3_event_params) { json_fixture('s3_event')['Records'] }
+      subject { instance.do }
 
-    before do |example|
-      allow_any_instance_of(Kvstore).to receive(:trigger_event).and_return true
-      instance.do if example.metadata[:do_before]
-    end
+      before do |example|
+        creator = FactoryGirl.create :user, mkey: 'ZcAK4dM9S4m0IFui6ok6'
+        target  = FactoryGirl.create :user, mkey: 'lpb8DcispONUSfdMOT9g'
+        FactoryGirl.create :push_user, mkey: target.mkey, push_token: 'qq64zz709r4zw1l6ap5p'
+        FactoryGirl.create :established_connection, creator: creator, target: target
+        allow_any_instance_of(Kvstore).to receive(:trigger_event).and_return true
+        instance.do if example.metadata[:do_before]
+      end
 
-    it { expect(subject).to be true }
-    it { expect { subject }.to change { Kvstore.count }.by 1 }
-    it 'specific kvstore placed in database', :do_before do
-      expect = %w(ZcAK4dM9S4m0IFui6ok6-lpb8DcispONUSfdMOT9g-da6f35c931ea53de0e24fb4c76beb5f3-VideoIdKVKey 1444235919617)
-      expect(Kvstore.pluck(:key1, :key2).last).to eq expect
+      it { expect(subject).to be true }
+      it { expect { subject }.to change { Kvstore.count }.by 1 }
+      it 'specific kvstore placed in database', :do_before do
+        expect = %w(ZcAK4dM9S4m0IFui6ok6-lpb8DcispONUSfdMOT9g-da6f35c931ea53de0e24fb4c76beb5f3-VideoIdKVKey 1444235919617)
+        expect(Kvstore.pluck(:key1, :key2).last).to eq expect
+      end
     end
   end
 end
