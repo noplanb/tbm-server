9199a700db8db315220e8e43c871e874f02c50e4
Ivan Kornilov <vano468@gmail.com>
2015-10-14 22:55:12 +0300

    Enable papertrail logging also on staging

--- lib/write_log.rb
+++ lib/write_log.rb
@@ -2,7 +2,7 @@ class WriteLog
   def self.info(context, message, settings = {})
     klass_name = context.instance_of?(Class) ? context.name : context.class.name
     Rails.logger.tagged(klass_name) { Rails.logger.info message }
-    Rails.syslogger.info("[#{klass_name}] #{message}") if Rails.env.production?
+    Rails.syslogger.info("[#{klass_name}] #{message}") if %w(production staging).include? Rails.env
     Rollbar.send(settings[:rollbar], "[#{klass_name}] #{message}") if settings[:rollbar]
   end
 end
