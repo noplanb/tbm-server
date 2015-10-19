4ea8220d65ded8f197e505d055350fc9324dec82
Ivan Kornilov <vano468@gmail.com>
2015-10-19 13:10:56 +0300

    Refactor WriteLog class, add logging for kvstore_controller#set

--- lib/write_log.rb
+++ lib/write_log.rb
@@ -1,8 +1,32 @@
 class WriteLog
   def self.info(context, message, settings = {})
-    klass_name = context.instance_of?(Class) ? context.name : context.class.name
-    Rails.logger.tagged(klass_name) { Rails.logger.info message }
-    Rails.syslogger.info("[#{klass_name}] #{message}") if %w(production staging).include? Rails.env
-    Rollbar.send(settings[:rollbar], "[#{klass_name}] #{message}") if settings[:rollbar]
+    prefix = get_class_name context
+    logging_local   prefix, message
+    logging_syslog  prefix, message
+    logging_rollbar prefix, message, settings[:rollbar] if settings[:rollbar]
+  end
+
+  def self.debug(context, message)
+    prefix = "#{get_class_name context} [DEBUG]"
+    logging_local  prefix, message
+    logging_syslog prefix, message
+  end
+
+  private
+
+  def self.logging_local(prefix, message)
+    Rails.logger.tagged(prefix) { Rails.logger.info message }
+  end
+
+  def self.logging_syslog(prefix, message)
+    Rails.syslogger.info("[#{prefix}] #{message}") if %w(production staging).include? Rails.env
+  end
+
+  def self.logging_rollbar(prefix, message, method = :error)
+    Rollbar.send(method, "[#{prefix}] #{message}") if Rollbar.respond_to? method
+  end
+
+  def self.get_class_name(context)
+    context.instance_of?(Class) ? context.name : context.class.name
   end
 end
