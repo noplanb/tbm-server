1c70d30b31c8a974be18a5966e20c17c61ddd951
Ivan Kornilov <vano468@gmail.com>
2015-10-21 18:09:57 +0300

    Fire event when user clicks on app link (need to refactor)

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,23 +1,35 @@
 class LandingController < ApplicationController
   layout 'landing'
-  before_action :set_state
 
   def index
     render :invite
   end
 
   def invite
+    platform = :unknown
+    redirect_path = nil
+
     if android? && ios?
+      platform = :mobile_device
+      redirect_path = root_path
       Rollbar.warning('Both iOS and Android detected')
-      redirect_to root_path
     elsif android?
-      redirect_to android_store_url
+      platform = :android
+      redirect_path = android_store_url
     elsif ios?
-      redirect_to iphone_store_url
+      platform = :ios
+      redirect_path = iphone_store_url
     elsif mobile_device?
+      platform = :mobile_device
+      redirect_path = root_path
       Rollbar.warning('Unsupported User Agent detected')
-      redirect_to root_path
     end
+
+    fire_app_link_clicked_event platform
+    redirect_to redirect_path if redirect_path
+
+    # todo: implement service like this
+    # Landing::HandleAppLinkClickedEvent.new(request) { |path| redirect_to path }
   end
 
   def privacy
@@ -28,7 +40,17 @@ class LandingController < ApplicationController
 
   private
 
-  def set_state
-    @inviter = params[:id] && User.where(mkey: params[:id]).first
+  def fire_app_link_clicked_event(platform)
+    connection = Connection.find_by_id params[:id]
+    EventDispatcher.emit(%w(user app_link_clicked),
+      initiator: 'user',
+      data: {
+        connection_id: connection.id,
+        connection_creator_mkey: connection.creator.mkey,
+        connection_target_mkey: connection.target.mkey,
+        platform: platform
+      },
+      raw_params: { user_agent: request.user_agent }
+    ) if connection
   end
 end
