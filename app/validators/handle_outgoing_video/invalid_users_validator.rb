7574e5bd09eadfc5d2daeb2f6d5c06bc3cb80653
Ivan Kornilov <vano468@gmail.com>
2016-01-19 18:21:31 +0300

    Disable invalid user alerting to rollbar

--- app/validators/handle_outgoing_video/invalid_users_validator.rb
+++ app/validators/handle_outgoing_video/invalid_users_validator.rb
@@ -6,7 +6,7 @@ class HandleOutgoingVideo::InvalidUsersValidator < ActiveModel::Validator
     users_not_found += "#{record.s3_metadata.receiver_mkey}[PushUser];" unless record.receiver_push_user
 
     unless users_not_found.empty?
-      HandleOutgoingVideo::StatusNotifier.new(record).rollbar :users_not_found, users: users_not_found
+      #HandleOutgoingVideo::StatusNotifier.new(record).rollbar :users_not_found, users: users_not_found
       record.errors.add :users, "these users are not found: #{users_not_found}"
     end
   end
