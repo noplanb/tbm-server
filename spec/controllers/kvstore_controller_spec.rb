9f23da652e725b391ed2045a276f0ccefab3e9e3
Ivan Kornilov <vano468@gmail.com>
2016-06-27 17:23:37 +0300

    Fix kvstore controller specs

--- spec/controllers/kvstore_controller_spec.rb
+++ spec/controllers/kvstore_controller_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe KvstoreController, type: :controller do
+  let(:user) { create(:user) }
   let(:message_id) { '1426622544176' }
   let(:connection) { create(:established_connection) }
   let(:sender) { connection.creator }
   let(:receiver) { connection.target }
+
+  def authenticate_user
+    authenticate_with_http_digest(user.mkey, user.auth) { yield }
+  end
 
   describe 'POST #set' do
     context 'video message' do
@@ -15,12 +20,18 @@ RSpec.describe KvstoreController, type: :controller do
 
       it do
         expect(Kvstore).to receive(:create_or_update).with(params)
-        post :set, params
+        authenticate_user { post :set, params }
       end
 
       it 'returns http success' do
-        post :set, params
+        authenticate_user { post :set, params }
         expect(response).to have_http_status(:success)
+      end
+
+      context 'when not authenticated' do
+        before { post :set, params }
+
+        it { is_expected.to respond_with(:unauthorized) }
       end
     end
 
@@ -32,44 +43,45 @@ RSpec.describe KvstoreController, type: :controller do
 
       it do
         expect(Kvstore).to receive(:create_or_update).with(params)
-        post :set, params
+        authenticate_user { post :set, params }
       end
 
       it 'returns http success' do
-        post :set, params
+        authenticate_user { post :set, params }
         expect(response).to have_http_status(:success)
+      end
+
+      context 'when not authenticated' do
+        before { post :set, params }
+
+        it { is_expected.to respond_with(:unauthorized) }
       end
     end
   end
 
   describe 'GET #delete' do
     it 'returns http success' do
-      get :delete
+      authenticate_user { get :delete }
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET #received_videos' do
     context 'when authenticated' do
-      let(:user) { create(:user) }
-
       it do
-        authenticate_with_http_digest(user.mkey, user.auth) do
-          get :received_videos
-        end
+        authenticate_user { get :received_videos }
         is_expected.to respond_with(:success)
       end
 
       it do
         expect_any_instance_of(User).to receive(:received_videos)
-        authenticate_with_http_digest(user.mkey, user.auth) do
-          get :received_videos
-        end
+        authenticate_user { get :received_videos }
       end
     end
 
     context 'when not authenticated' do
       before { get :received_videos }
+
       it { is_expected.to respond_with(:unauthorized) }
     end
   end
@@ -79,22 +91,19 @@ RSpec.describe KvstoreController, type: :controller do
       let(:user) { create(:user) }
 
       it do
-        authenticate_with_http_digest(user.mkey, user.auth) do
-          get :received_texts
-        end
+        authenticate_user { get :received_texts }
         is_expected.to respond_with(:success)
       end
 
       it do
         expect_any_instance_of(User).to receive(:received_texts)
-        authenticate_with_http_digest(user.mkey, user.auth) do
-          get :received_texts
-        end
+        authenticate_user { get :received_texts }
       end
     end
 
     context 'when not authenticated' do
       before { get :received_videos }
+
       it { is_expected.to respond_with(:unauthorized) }
     end
   end
@@ -104,22 +113,19 @@ RSpec.describe KvstoreController, type: :controller do
       let(:user) { create(:user) }
 
       it do
-        authenticate_with_http_digest(user.mkey, user.auth) do
-          get :video_status
-        end
+        authenticate_user { get :video_status }
         is_expected.to respond_with(:success)
       end
 
       it do
         expect_any_instance_of(User).to receive(:video_status)
-        authenticate_with_http_digest(user.mkey, user.auth) do
-          get :video_status
-        end
+        authenticate_user { get :video_status }
       end
     end
 
     context 'when not authenticated' do
       before { get :video_status }
+
       it { is_expected.to respond_with(:unauthorized) }
     end
   end
