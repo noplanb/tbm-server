ad792b276a4e069ef3b25b45fe77f100cf85310b
Ivan Kornilov <vano468@gmail.com>
2016-07-15 20:03:20 +0300

    Add specs for Messages::Get::Message interactor

---
+++ spec/interactors/messages/get/message_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Messages::Get::Message do
+  let(:user) { create(:user) }
+  let(:message) do
+    friend = create(:user)
+    create(:established_connection, creator: user, target: friend)
+    Kvstore.add_message_id_key('video', friend, user, gen_message_id)
+  end
+  let(:default_params) { { user: user, id: message.key2 } }
+
+  describe '.run' do
+    subject { described_class.run(default_params) }
+
+    it { expect(subject.valid?).to be(true) }
+    it { expect(subject.result).to eq(message) }
+  end
+
+  describe 'validations' do
+    subject { described_class.run(params).errors.full_messages }
+
+    context 'presence' do
+      let(:params) { default_params.merge(id: '123456789100') }
+
+      it { is_expected.to eq(['Message not found by key2=123456789100']) }
+    end
+
+    context 'ownership' do
+      let(:params) { default_params.merge(user: create(:user)) }
+
+      it { is_expected.to eq(['Message not associated with user']) }
+    end
+  end
+end
