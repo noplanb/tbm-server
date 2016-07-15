35fadd3764cb2560f1fd039b2250dcda693dabcf
Ivan Kornilov <vano468@gmail.com>
2016-07-15 19:34:48 +0300

    Add some new specs for messages interactors

---
+++ spec/interactors/messages/get/type_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Messages::Get::Type do
+  let(:default_params) { { type: 'video' } }
+
+  describe '.run' do
+    subject { described_class.run(default_params) }
+
+    it { expect(subject.valid?).to be(true) }
+    it { expect(subject.result).to eq('video') }
+  end
+
+  describe 'validations' do
+    subject { described_class.run(params).errors.full_messages }
+
+    context 'inclusion' do
+      let(:params) { { type: 'incorrect' } }
+
+      it { is_expected.to eq(['Type incorrect is not allowed']) }
+    end
+  end
+end
