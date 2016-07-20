85759329dd512b879fc551f2c02b2b8550574828
Ivan Kornilov <vano468@gmail.com>
2016-07-20 14:18:00 +0300

    Add authentication and new specs for version controller

--- app/controllers/version_controller.rb
+++ app/controllers/version_controller.rb
@@ -1,4 +1,6 @@
 class VersionController < ApplicationController
+  before_action :authenticate
+
   def check_compatibility
     version_compatibility = VersionCompatibility.instance
     Users::SaveDeviceInfo.run(user: current_user, platform: params[:device_platform], version: params[:version])
