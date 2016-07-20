75924813add7ac17d725ff4984208cb2d4effa59
Ivan Kornilov <vano468@gmail.com>
2016-07-20 13:17:53 +0300

    Implement Users::SaveDeviceInfo interactor

--- app/controllers/version_controller.rb
+++ app/controllers/version_controller.rb
@@ -1,8 +1,7 @@
 class VersionController < ApplicationController
   def check_compatibility
-    #Users::SaveDeviceInfo.run(user: current_user, platform: params[:device_platform], version: params[:version])
     version_compatibility = VersionCompatibility.instance
-    render json: { result: version_compatibility.compatibility(params[:device_platform],
-                                                               params[:version]) }
+    Users::SaveDeviceInfo.run(user: current_user, platform: params[:device_platform], version: params[:version])
+    render json: { result: version_compatibility.compatibility(params[:device_platform], params[:version]) }
   end
 end
---
+++ app/interactions/users/save_device_info.rb
@@ -0,0 +1,24 @@
+class Users::SaveDeviceInfo < ActiveInteraction::Base
+  ALLOWED_PLATFORMS = %w(ios android)
+
+  object :user
+  string :platform
+  string :version
+
+  validates :platform, inclusion: { in: ALLOWED_PLATFORMS,
+                                    message: '%{value} is not allowed' }
+
+  def execute
+    update_attributes if update_required?
+  end
+
+  private
+
+  def update_required?
+    user.device_platform != platform || user.app_version != version
+  end
+
+  def update_attributes
+    user.update_attributes(device_platform: platform, app_version: version)
+  end
+end
