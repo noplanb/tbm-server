ce60a1d67c2552ebe869ab734b4c7a5924c8fab6
Ivan Kornilov <vano468@gmail.com>
2016-08-31 14:51:35 +0300

    Update global phone database

--- app/validators/handle_outgoing_video/duplication_case_validator.rb
+++ app/validators/handle_outgoing_video/duplication_case_validator.rb
@@ -1,8 +1,8 @@
 class HandleOutgoingVideo::DuplicationCaseValidator < ActiveModel::Validator
   def validate(record)
-    if NotifiedS3Object.persisted? record.s3_event.file_name
-      HandleOutgoingVideo::StatusNotifier.new(record).rollbar :duplication
-      record.errors.add :file_name, 'already persisted in database, duplication case'
+    if NotifiedS3Object.persisted?(record.s3_event.file_name)
+      HandleOutgoingVideo::StatusNotifier.new(record).rollbar(:duplication)
+      record.errors.add(:file_name, 'already persisted in database, duplication case')
     end
   end
 end
